234b78bb908b074bdf226633012349408910c4ed
Lamanris <Dreamer19042002@gmail.com>
2020-11-15 21:57:07 +0600

    fix

--- client/components/readMe.js
+++ client/components/readMe.js
@@ -15,8 +15,8 @@ const ReadMe = ({ repositoryName, repoInfo, userName, repoList }) => {
         <h3 className="text-center text-2xl mt-3">
           Repository: <b>{repositoryName}</b>
         </h3>
-        <div className="bg-blue-500 inline-block rounded p-1 hover:bg-blue-400 text-center w-1/5 mt-3 mb-5">
-          <a href={`https://github.com/${repositoryName}`}>GitHub Link</a>
+        <div className="w-1/5">
+          <a className='bg-blue-500 inline-block rounded p-1 hover:bg-blue-400 text-center mt-3 mb-5 cursor-pointer w-full' href={`https://github.com/${userName}/${repositoryName}`}>GitHub Link</a>
         </div>
         <div className="markdown-body">
           <Markdown>{repoInfo}</Markdown>
